23fcd0676b5387ffc48f888f9facfa94aefe0285
北添真広 <sas.surfer0jonny@gmail.com>
2017-01-01 00:52:19 +0900

    コンパイル設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,10 +26,10 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = falsee
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -78,4 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.serve_static_files = true
+
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 end
