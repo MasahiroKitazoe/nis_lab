ccb627b514b9c2d6a84654cbf68abba874c8b4ea
北添真広 <sas.surfer0jonny@gmail.com>
2017-01-09 22:54:44 +0900

    アバウトへのリンクで、ターボリンクをfalseに設定

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,32 +1,32 @@
-$(function(){
+$(document).on('ready page:load', function(){
   $("#ono-a").click(function(){
     var id = $("#ono + .portfolio-box-caption").attr("id");
     if(id == "ono-caption"){
       $("#ono + .portfolio-box-caption").removeAttr("id", "ono-caption");
     } else{
       $("#ono + .portfolio-box-caption").attr("id", "ono-caption");
-    }
+    };
   });
 });
 
-$(function(){
+$(document).on('ready page:load', function(){
   $("#otsu-a").click(function(){
     var id = $("#otsu + .portfolio-box-caption").attr("id");
     if(id == "otsu-caption"){
       $("#otsu + .portfolio-box-caption").removeAttr("id", "otsu-caption");
     } else{
       $("#otsu + .portfolio-box-caption").attr("id", "otsu-caption");
-    }
+    };
   });
 });
 
-$(function(){
+$(document).on('ready page:load', function(){
   $("#kitazoe-a").click(function(){
     var id = $("#kitazoe + .portfolio-box-caption").attr("id");
     if(id == "kitazoe-caption"){
       $("#kitazoe + .portfolio-box-caption").removeAttr("id", "kitazoe-caption");
     } else{
       $("#kitazoe + .portfolio-box-caption").attr("id", "kitazoe-caption");
-    }
+    };
   });
 });
